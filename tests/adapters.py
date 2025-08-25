2b678fbcb3febfc7c27857ef717874e69480fb9e
cheesepapa <cheesepapa@cheesepapadebijibendiannao.local>
2025-08-25 21:35:22 +0800

    response log probs

--- tests/adapters.py
+++ tests/adapters.py
@@ -8,7 +8,7 @@ from torch import Tensor
 from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizerBase
 
-from cs336_alignment.util import compute_entropy, tokenize_prompt_and_output
+from cs336_alignment.util import compute_entropy, get_response_log_probs, tokenize_prompt_and_output
 
 
 def run_tokenize_prompt_and_output(
@@ -116,7 +116,7 @@ def run_get_response_log_probs(
                 we have not masked out the token indices corresponding to the prompt
                 or padding; that is done in the train loop.
     """
-    raise NotImplementedError
+    return get_response_log_probs(model, input_ids, labels, return_token_entropy)
 
 
 def run_compute_naive_policy_gradient_loss(
