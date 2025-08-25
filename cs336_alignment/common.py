865131b381c41996591a791404c397e99ba48b23
cheesepapa <cheesepapa@cheesepapadebijibendiannao.local>
2025-08-25 21:42:20 +0800

    moved helper methods to common.py

--- cs336_alignment/common.py
+++ cs336_alignment/common.py
@@ -1,8 +1,12 @@
 import json
 import os
+import torch
 from pathlib import Path
 from typing import Callable, List, Tuple
 from vllm import LLM, SamplingParams
+from transformers import PreTrainedTokenizerBase, AutoModelForCausalLM, AutoTokenizer
+from torch.nn.utils.rnn import pad_sequence
+from torch.nn.functional import softmax
 
 
 # project root directory
@@ -127,3 +131,128 @@ def load_test_set() -> List[dict]:
         test_set = [json.loads(line) for line in f]
 
     return test_set
+
+
+def tokenize_prompt_and_output(
+    prompt_strs: list[str],
+    output_strs: list[str],
+    tokenizer: PreTrainedTokenizerBase,
+) -> dict[str, torch.Tensor]:
+    """Tokenize the prompt and output strings, and construct a mask that is 1
+    for the response tokens and 0 for other tokens (prompt or padding).
+
+    Args:
+        prompt_strs: list[str], the prompt strings.
+        output_strs: list[str], the output strings.
+        tokenizer: PreTrainedTokenizer, the tokenizer to use.
+
+    Returns:
+        dict[str, torch.Tensor]:
+            "input_ids": torch.Tensor of shape (batch_size, max(prompt_and_output_lens) - 1):
+                the tokenized prompt and output strings, with the final token sliced off.
+            "labels": torch.Tensor of shape (batch_size, max(prompt_and_output_lens) - 1):
+                shifted input_ids (i.e., the input_ids without the first token).
+            "response_mask": torch.Tensor of shape (batch_size, max(prompt_and_output_lens) - 1):
+                a mask on the response tokens in `labels`.
+    """
+    input_ids = []
+    response_mask = []
+
+    prompt_inputs = tokenizer(prompt_strs, add_special_tokens=False)
+    output_inputs = tokenizer(output_strs, add_special_tokens=False)
+
+    for i in range(len(prompt_strs)):
+        input_ids.append(
+            torch.tensor(
+                prompt_inputs["input_ids"][i] + 
+                output_inputs["input_ids"][i]
+            )
+        )
+        response_mask.append(
+            torch.tensor(
+                [0] * len(prompt_inputs["input_ids"][i]) + 
+                [1] * len(output_inputs["attention_mask"][i])
+            )
+        )
+    
+    input_ids = pad_sequence(input_ids, batch_first=True, padding_value=tokenizer.pad_token_id)
+    response_mask = pad_sequence(response_mask, batch_first=True, padding_value=0)
+
+    return {
+        "input_ids": input_ids[:, :-1],
+        "labels": input_ids[:, 1:],
+        "response_mask": response_mask[:, 1:] > 0,
+    }
+
+
+def compute_entropy(logits: torch.Tensor) -> torch.Tensor:
+    """Get the entropy of the logits (i.e., entropy of the final dimension)."""
+
+    log_sum_exp = torch.logsumexp(logits, dim=2, keepdim=True)
+    log_probs = logits - log_sum_exp
+    entropy = -torch.sum(torch.exp(log_probs) * log_probs, dim=2)
+    return entropy
+
+
+def get_response_log_probs(
+    model: torch.nn.Module,
+    input_ids: torch.Tensor,
+    labels: torch.Tensor,
+    return_token_entropy: bool,
+) -> torch.Tensor:
+    """Get the conditional log-probs of the response given the prompt,
+        and optionally the entropy of the next token predictions.
+
+    Args:
+        model: PreTrainedModel, the model to score.
+        input_ids: torch.Tensor of shape (batch_size, sequence_length):
+            the tokenized prompt and output.
+        labels: torch.Tensor of shape (batch_size, sequence_length):
+            shifted input_ids.
+        return_token_entropy: bool, whether to return the entropy of the
+            next token predictions.
+
+    Returns:
+        dict[str, torch.Tensor]:
+            "log_probs": torch.Tensor of shape (batch_size, sequence_length):
+                the conditional log-probs of the response given the prompt.
+                Note that we have not masked out the token indices corresponding
+                to the prompt or padding; that is done in the train loop.
+            "token_entropy": Optional[torch.Tensor] of shape (batch_size, sequence_length):
+                the entropy of the next token predictions. As with the log-probs,
+                we have not masked out the token indices corresponding to the prompt
+                or padding; that is done in the train loop.
+    """
+    logits = model(input_ids).logits
+    log_probs = torch.log_softmax(logits, dim=2)
+    
+    if return_token_entropy:
+        entropy = compute_entropy(logits)
+        return {
+            "log_probs": log_probs[:, -1, :],
+            "token_entropy": entropy,
+        }
+    else:
+        return {
+            "log_probs": log_probs[:, -1, :],
+    }
+
+
+if __name__ == "__main__":
+    tokenization_result = tokenize_prompt_and_output(
+        ['Hello, world!', 'This is a test.', 'This is another test.'],
+        ['Hello, world!', 'This is a test.', 'This is another test.'],
+        AutoTokenizer.from_pretrained("Qwen/Qwen2.5-Math-1.5B"),
+    )
+    input_ids = tokenization_result["input_ids"]
+    labels = tokenization_result["labels"]
+    response_mask = tokenization_result["response_mask"]
+    
+    model = AutoModelForCausalLM.from_pretrained(
+        "Qwen/Qwen2.5-Math-1.5B",
+        torch_dtype=torch.bfloat16,
+    )
+    logits = model(input_ids).logits
+    print(logits.shape)
+    
+    
